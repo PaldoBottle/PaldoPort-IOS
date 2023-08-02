c16e7f16f4b24f828201097f695d5556bc60fad8
seonwoonam <potatomoney716@gmail.com>
2023-08-03 00:17:48 +0900

    FEAT : 메인 지도 페이지 구현

---
+++ PaldoPort/PaldoPort/Pages/Map/MapViewController.swift
@@ -0,0 +1,95 @@
+//
+//  ViewController.swift
+//  PaldoPort
+//
+//  Created by seonwoo on 2023/08/02.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class MapViewController: UIViewController {
+    
+    @IBOutlet weak var pardoPortLabel: UILabel!
+    @IBOutlet weak var mapView: MKMapView!
+    var locationManager: CLLocationManager!
+    
+    let viewModel = MapViewModel()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.sendSubviewToBack(mapView)
+        view.bringSubviewToFront(pardoPortLabel)
+
+        getUserLocationInfo()
+        mapInit()
+        annotationInit()
+    }
+    
+    func getUserLocationInfo(){
+        // user location 정보 받아오기
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        self.locationManager.requestWhenInUseAuthorization()
+    }
+
+    func mapInit(){
+        // 기본지도
+        mapView.preferredConfiguration = MKStandardMapConfiguration()
+        
+        mapView.mapType = MKMapType.standard
+        
+        // 줌 가능 여부
+        mapView.isZoomEnabled = true
+        // 이동 가능 여부
+        mapView.isScrollEnabled = true
+        // 회전 가능 여부
+        mapView.isRotateEnabled = true
+        // 나침판 표시 여부
+        mapView.showsCompass = true
+        // 위치 사용 시 사용자의 현재 위치를 표시
+        mapView.showsUserLocation = true
+        
+        mapView.setUserTrackingMode(.follow, animated: true)
+        
+    }
+    
+    func annotationInit(){
+        
+        for annotation in viewModel.annotations {
+            let pin = MKPointAnnotation()
+        
+            pin.coordinate = CLLocationCoordinate2D(latitude: annotation.latitude, longitude: annotation.longitude)
+            pin.title = annotation.locationName
+            mapView.addAnnotation(pin)
+        }
+    }
+
+}
+
+extension MapViewController: CLLocationManagerDelegate {
+    
+    func getLocationUsagePermission() {
+        //location4
+        self.locationManager.requestWhenInUseAuthorization()
+
+    }
+
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        //location5
+        switch status {
+        case .authorizedAlways, .authorizedWhenInUse:
+            print("GPS 권한 설정됨")
+        case .restricted, .notDetermined:
+            print("GPS 권한 설정되지 않음")
+            getLocationUsagePermission()
+        case .denied:
+            print("GPS 권한 요청 거부됨")
+            getLocationUsagePermission()
+        default:
+            print("GPS: Default")
+        }
+    }
+}
---
+++ PaldoPort/PaldoPort/Pages/Map/MapViewModel.swift
@@ -0,0 +1,23 @@
+//
+//  MapViewModel.swift
+//  PaldoPort
+//
+//  Created by seonwoo on 2023/08/02.
+//
+
+import Foundation
+
+class MapViewModel{
+    
+    var annotations : [Annotation] = []
+    
+    init(){
+        annotations = [
+            Annotation(id: 1, latitude: 37.57861, longitude: 126.97722, locationName: "경복궁"),
+            Annotation(id: 2, latitude: 37.570013917406, longitude: 126.9780542555, locationName: "광화문"),
+            Annotation(id: 0, latitude: 37.567191, longitude: 127.010490, locationName: "DDP"),
+        ]
+    }
+    
+    
+}

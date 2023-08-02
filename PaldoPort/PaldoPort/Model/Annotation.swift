fc5d420bc4cdddf358b6e87747c77ca1c71f10a5
seonwoonam <potatomoney716@gmail.com>
2023-08-03 00:17:31 +0900

    FEAT : annotation 모델 추가

---
+++ PaldoPort/PaldoPort/Model/Annotation.swift
@@ -0,0 +1,19 @@
+//
+//  Annotation.swift
+//  PaldoPort
+//
+//  Created by seonwoo on 2023/08/02.
+//
+
+import Foundation
+
+
+struct Annotation : Decodable{
+    var id : Int
+    // 위도
+    var latitude : Double
+    // 경도
+    var longitude : Double
+    var locationName : String
+    
+}

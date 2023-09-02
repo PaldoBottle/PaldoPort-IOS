48c9a1226be960d5a99b60e788b005b4706058af
seonwoonam <potatomoney716@gmail.com>
2023-09-03 02:00:52 +0900

    [FEAT] : API 주소 추가

--- PaldoPort/PaldoPort/Resource/Constants/APIConstants.swift
+++ PaldoPort/PaldoPort/Resource/Constants/APIConstants.swift
@@ -13,5 +13,23 @@ struct APIConstants {
     
     static let loginWithGoogle = baseURL + "/login/google"
     
+    static let logout = baseURL + ""
+    static let getUser = baseURL + "/memberinfo"
+    
+    // MARK: - Challenge
+    static let getAchievedChallenge = baseURL + "/challenge/achieve"
+    static let getAllChallengeList = baseURL + "/challenge/list"
+    
+    // MARK: - Stamp
+    static let getAllStamp = baseURL + "/stamp/user/list"
+    static let issueStamp = baseURL + "/stamp/user/new"
+    static let getStampDetail = baseURL + "/stamp"
+    
+    // MARK: - Region
+    
+    // 뒤에 추가로 더붙야함
+    static let getRegion = baseURL + "/region"
+    static let getAllAnnotation = baseURL + "/region/landmark/list"
+    
 }
 

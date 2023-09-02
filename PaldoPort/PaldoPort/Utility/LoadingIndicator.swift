af6abf8704c6fd28539e90760fe3b89b2aa7c1c9
seonwoonam <potatomoney716@gmail.com>
2023-09-02 23:33:46 +0900

    [FEAT] : 로딩중 추가

---
+++ PaldoPort/PaldoPort/Utility/LoadingIndicator.swift
@@ -0,0 +1,31 @@
+import Foundation
+import UIKit
+
+class LoadingIndicator {
+    static func showLoading() {
+        DispatchQueue.main.async {
+            // 최상단에 있는 window 객체 획득
+            guard let window = UIApplication.shared.windows.last else { return }
+
+            let loadingIndicatorView: UIActivityIndicatorView
+            if let existedView = window.subviews.first(where: { $0 is UIActivityIndicatorView } ) as? UIActivityIndicatorView {
+                loadingIndicatorView = existedView
+            } else {
+                loadingIndicatorView = UIActivityIndicatorView(style: .large)
+                /// 다른 UI가 눌리지 않도록 indicatorView의 크기를 full로 할당
+                loadingIndicatorView.frame = window.frame
+                loadingIndicatorView.color = UIColor.mainColor
+                window.addSubview(loadingIndicatorView)
+            }
+
+            loadingIndicatorView.startAnimating()
+        }
+    }
+
+    static func hideLoading() {
+        DispatchQueue.main.async {
+            guard let window = UIApplication.shared.windows.last else { return }
+            window.subviews.filter({ $0 is UIActivityIndicatorView }).forEach { $0.removeFromSuperview() }
+        }
+    }
+}

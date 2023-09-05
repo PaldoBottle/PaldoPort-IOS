e32a15ef3d190cb5176ee218a1faa20c2e6cf6b7
seonwoonam <potatomoney716@gmail.com>
2023-09-06 05:11:42 +0900

    [FEAT] : navigation view controller 추가

---
+++ PaldoPort/PaldoPort/Pages/Map/MapNavigationViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MapViewNavigationController.swift
+//  PaldoPort
+//
+//  Created by seonwoo on 2023/09/04.
+//
+
+import Foundation
+import UIKit
+
+class MapNavigationViewController : UINavigationController{
+    
+}

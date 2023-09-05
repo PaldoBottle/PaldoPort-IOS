9478ad3695c17cadbf4d607a9c3a4bd46c42a53f
seonwoonam <potatomoney716@gmail.com>
2023-09-06 02:31:35 +0900

    [FEAT] : collectionview 추가

---
+++ PaldoPort/PaldoPort/Utility/CollectionView/ChallengeCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ChallengeCollectionView.swift
+//  PaldoPort
+//
+//  Created by seonwoo on 2023/09/04.
+//
+
+import Foundation
+import UIKit
+
+
+class ChallengeCollectionViewCell : UICollectionViewCell {
+    
+    @IBOutlet weak var point: UILabel!
+    @IBOutlet weak var desc: UILabel!
+    @IBOutlet weak var name: UILabel!
+}
---
+++ PaldoPort/PaldoPort/Utility/CollectionView/MyCollectionViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MyCollectionViewCell.swift
+//  PaldoPort
+//
+//  Created by seonwoo on 2023/09/04.
+//
+
+import Foundation
+import UIKit
+import Kingfisher
+
+
+class MyCollectionViewCell : UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var name: UILabel!
+    
+    @IBOutlet weak var district: UILabel!
+    
+}
+
+

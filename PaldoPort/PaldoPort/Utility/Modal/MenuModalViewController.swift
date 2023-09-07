f51eb127ca6501132c80114d395232ae26802c60
seonwoonam <potatomoney716@gmail.com>
2023-09-07 21:50:05 +0900

    [FIX] : 메뉴 모달 수정

--- PaldoPort/PaldoPort/Utility/Modal/MenuModalViewController.swift
+++ PaldoPort/PaldoPort/Utility/Modal/MenuModalViewController.swift
@@ -6,11 +6,14 @@
 //
 
 import UIKit
+import Kingfisher
 
 
 class MenuModalViewController: UIViewController, UITableViewDelegate, UITableViewDataSource
 {
-    let data = ["설정 및 개인정보", "내 스탬프", "챌린지", "로그아웃"]
+    let data = ["내 스탬프", "챌린지"]
+    var gotoStampPage: (() -> Void)?
+    var gotoChallengePage : (() -> Void)?
     
     var menuView : UITableView!
     
@@ -25,8 +28,37 @@ class MenuModalViewController: UIViewController, UITableViewDelegate, UITableVie
             sheet.prefersGrabberVisible = true
         }
         
+        let imageView = UIImageView(frame: CGRect(x: 0, y: 50, width: 140, height: 140))
+        
+        let user : User = UserManager.shared.getUser()!
+        
+        let url = URL(string: user.profileImg!)
+        imageView.kf.setImage(with:url)
+        
+        imageView.layer.cornerRadius = imageView.frame.height/2
+//        imageView.image = UIImage(named: "paldoPort")
+        imageView.layer.borderWidth = 1.0
+        imageView.layer.borderColor = UIColor.lightGray.cgColor
+        imageView.center.x = view.center.x
+            imageView.contentMode = .scaleAspectFill
+            imageView.clipsToBounds = true
+
+            self.view.addSubview(imageView)
+        
+        let nameLabel = UILabel(frame: CGRect(x: 0, y: imageView.frame.maxY + 10, width: view.frame.width, height: 30))
+        nameLabel.text = user.id
+        nameLabel.textAlignment = .center
+
+        let dateLabel = UILabel(frame: CGRect(x: 0, y: nameLabel.frame.maxY + 5, width: view.frame.width, height: 20))
+        dateLabel.text = "내 포인트 : \(user.point!)P"
+        dateLabel.textAlignment = .center
+        dateLabel.textColor = UIColor.gray
+
+        view.addSubview(nameLabel)
+        view.addSubview(dateLabel)
+        
         self.menuView = UITableView(frame: CGRect(
-            x: 0, y:0, width: self.view.bounds.width,
+            x: 0, y:dateLabel.frame.maxY + 2, width: self.view.bounds.width,
             height: self.view.bounds.height
         ))
         
@@ -35,7 +67,7 @@ class MenuModalViewController: UIViewController, UITableViewDelegate, UITableVie
         
         self.menuView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
         
-        let topPadding: CGFloat = 40.0
+        let topPadding: CGFloat = 20.0
               menuView.contentInset = UIEdgeInsets(top: topPadding, left: 0, bottom: 0, right: 0)
 
         self.view.addSubview(menuView)
@@ -57,27 +89,17 @@ class MenuModalViewController: UIViewController, UITableViewDelegate, UITableVie
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
 
-        let loginPageStoryboard : UIStoryboard = UIStoryboard(name: "MapView", bundle: nil)
+        let mapPageStoryboard : UIStoryboard = UIStoryboard(name: "MapView", bundle: nil)
 
         tableView.deselectRow(at: indexPath, animated: true)
 
         switch indexPath.row {
         case 0:
-            break
-        case 1:
-            print("흠")
-            if let targetViewController = storyboard?.instantiateViewController(withIdentifier: "MapNavigationViewController") {
-                print("애초에 여길안오나?")
-                if let navigationController = targetViewController.navigationController {
-                    guard let viewController = UIStoryboard(name: "MyStamp", bundle: nil).instantiateViewController(identifier: "MyStampViewController") as? MyStampViewController else {return}
-                    navigationController.pushViewController(viewController, animated: true)
-                }
-                print("여기까지 안오나")
-            }
             self.dismiss(animated: true)
-        case 2:
-            guard let viewController = UIStoryboard(name: "Challenge", bundle: nil).instantiateViewController(identifier: "ChallengeViewController") as? ChallengeViewController else {return}
-            self.navigationController?.pushViewController(viewController, animated: true)
+            self.gotoStampPage!()
+        case 1:
+            self.dismiss(animated: true)
+            self.gotoChallengePage!()
         default:
             return
         }
